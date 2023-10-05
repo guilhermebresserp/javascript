9591675e00bfbf9f38f430e37d1e4e0ee1b760a6
Guilherme Bresser Pereira <131826098+guilhermebresserp@users.noreply.github.com>
2023-10-05 02:18:45 -0300

    Completei a aula 12

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,7 +1,8 @@
-var hora = 3
+var agora = new Date()
+var hora = agora.getHours()
 console.log(`Agora são exatamente ${hora} horas`)
 if (hora < 6) {
-    console.log('Bom madrugada!')
+    console.log('Boa madrugada!')
 } else if (hora < 12){
     console.log('Bom dia!')
 } else if ( hora < 18){
